ab44b648014594caa151a338202cfd4fd502316b
Francis Prevail <prevailfrancis@gmail.com>
2026-09-23 12:40:40 +0100

    Fix BASE_DIR path in settings.py

--- finance/settings.py
+++ finance/settings.py
@@ -16,7 +16,7 @@ from urllib.parse import urlparse
 # ==========================
 # Base & Environment
 # ==========================
-BASE_DIR = Path(__file__).resolve().parent.par[]
+BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.environ.get(
